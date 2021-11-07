0bcf430a36f8bbdfb5947bf761928a0e40321ea6
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-11-07 13:26:11 +0100

    fix: update env var checking

--- src/next-edge/index.ts
+++ src/next-edge/index.ts
@@ -58,8 +58,17 @@ export function createApiHandler(options: CreateApiHandlerOptions) {
   let baseUrl = options.fallbackToPlayground
     ? 'https://playground.projects.oryapis.com/'
     : ''
+
+  if (process.env.ORY_SDK_URL) {
+    baseUrl = process.env.ORY_SDK_URL
+  }
+
   if (process.env.ORY_KRATOS_URL) {
     baseUrl = process.env.ORY_KRATOS_URL
+  }
+
+  if (process.env.ORY_SDK_URL && process.env.ORY_KRATOS_URL) {
+    throw new Error('Only one of ORY_SDK_URL or ORY_KRATOS_URL can be set.')
   }
 
   if (options.apiBaseUrlOverride) {
