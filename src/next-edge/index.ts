bad765b595610265daaacb49001736142899f08d
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-02-06 10:16:12 +0100

    refactor: move away from ory/kratos-client

--- src/next-edge/index.ts
+++ src/next-edge/index.ts
@@ -5,6 +5,48 @@ import parse from 'set-cookie-parser'
 import { IncomingHttpHeaders } from 'http'
 import { Buffer } from 'buffer'
 import { isText } from 'istextorbinary'
+import { NextResponse } from 'next/server'
+import type { NextMiddleware } from 'next/server'
+
+const forwardedHeaders = [
+  'accept',
+  'accept-charset',
+  'accept-encoding',
+  'accept-language',
+  'authorization',
+  'cache-control',
+  'content-type',
+  'cookie',
+  'host',
+  'user-agent',
+  'referer'
+]
+
+const encode = (v: string) => v
+
+function getBaseUrl(options: CreateApiHandlerOptions) {
+  let baseUrl = options.fallbackToPlayground
+    ? 'https://playground.projects.oryapis.com/'
+    : ''
+
+  if (process.env.ORY_SDK_URL) {
+    baseUrl = process.env.ORY_SDK_URL
+  }
+
+  if (process.env.ORY_KRATOS_URL) {
+    baseUrl = process.env.ORY_KRATOS_URL
+  }
+
+  if (process.env.ORY_SDK_URL && process.env.ORY_KRATOS_URL) {
+    throw new Error('Only one of ORY_SDK_URL or ORY_KRATOS_URL can be set.')
+  }
+
+  if (options.apiBaseUrlOverride) {
+    baseUrl = options.apiBaseUrlOverride
+  }
+
+  return baseUrl.replace(/\/$/, '')
+}
 
 /**
  * The NextJS API configuration
@@ -14,8 +56,6 @@ export const config = {
     bodyParser: false
   }
 }
-
-const encode = (v: string) => v
 
 export interface CreateApiHandlerOptions {
   /**
@@ -52,45 +92,10 @@ export interface CreateApiHandlerOptions {
 /**
  * Creates a NextJS / Vercel API Handler
  *
- * For this handler to work, please set the environment variable `ORY_KRATOS_URL`.
+ * For this handler to work, please set the environment variable `ORY_SDK_URL`.
  */
 export function createApiHandler(options: CreateApiHandlerOptions) {
-  let baseUrl = options.fallbackToPlayground
-    ? 'https://playground.projects.oryapis.com/'
-    : ''
-
-  if (process.env.ORY_SDK_URL) {
-    baseUrl = process.env.ORY_SDK_URL
-  }
-
-  if (process.env.ORY_KRATOS_URL) {
-    baseUrl = process.env.ORY_KRATOS_URL
-  }
-
-  if (process.env.ORY_SDK_URL && process.env.ORY_KRATOS_URL) {
-    throw new Error('Only one of ORY_SDK_URL or ORY_KRATOS_URL can be set.')
-  }
-
-  if (options.apiBaseUrlOverride) {
-    baseUrl = options.apiBaseUrlOverride
-  }
-
-  baseUrl = baseUrl.replace(/\/$/, '')
-
-  const forwardedHeaders = [
-    'accept',
-    'accept-charset',
-    'accept-encoding',
-    'accept-language',
-    'authorization',
-    'cache-control',
-    'content-type',
-    'cookie',
-    'host',
-    'user-agent',
-    'referer'
-  ]
-
+  const baseUrl = getBaseUrl(options)
   return (req: NextApiRequest, res: NextApiResponse<string>) => {
     const { paths, ...query } = req.query
     const search = new URLSearchParams()
